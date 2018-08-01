53d782ae887fcc60d829f09b525e97f99fecc739
jdamador <amadorsalasjd@gmail.com>
2018-07-31 22:03:07 -0600

    feat: add exercise #2 V.1
    Se agregó el ejercicio dos donde se conecta un botón que funciona como interruptor para un led

---
+++ Ejercicios/2. PowerOn/PowerOn V.1/2._PowerOnOFF.ino
@@ -0,0 +1,19 @@
+//Práctica: encender y apagar un LED con un botón pulsador
+const int led=13; 
+const int boton=7; 
+
+int val=0; //Almacena el estado del pulsador
+void setup(){
+  pinMode(led,OUTPUT); //Se establece el pin como una señal de salida.
+  pinMode(boton,INPUT); //Se establece el pin como una señal de entrada.
+}
+
+void loop(){
+  val = digitalRead(boton); //Hace una lectura del estado del botón.
+  if(val==HIGH){ //Si esta presionado se enciende el led.
+    digitalWrite(led,HIGH);
+  }else{ //Si no, entoces se apaga.
+    digitalWrite(led,LOW);
+  }
+}
+
